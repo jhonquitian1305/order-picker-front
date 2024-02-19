d93cd9298c4982d04d6a6d7e76fd8b91679c8384
Jhon Quitian <jhon.quitian@udea.edu.co>
2024-02-19 15:51:36 -0500

    Feat: add method to get a user by email

--- src/app/users/services/user.service.ts
+++ src/app/users/services/user.service.ts
@@ -1,8 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from '../../../environments/environment';
-import { Observable } from 'rxjs';
-import { PaginationUser } from '../interfaces/user.interface';
+import { Observable, catchError, of } from 'rxjs';
+import { PaginationUser, User } from '../interfaces/user.interface';
 
 @Injectable({
   providedIn: 'root'
@@ -15,5 +15,11 @@ export class UserService {
 
   getAll(page: number, size: number): Observable<PaginationUser> {
     return this.http.get<PaginationUser>(`${this.baseUrl}/users?pageNumber=${page}&pageSize=${size}`);
+  }
+
+  getOneByEmail(value: string): Observable<User | undefined>{
+    return this.http.get<User>(`${this.baseUrl}/users/email/${value}`).pipe(
+      catchError( error => of(undefined))
+    )
   }
 }
